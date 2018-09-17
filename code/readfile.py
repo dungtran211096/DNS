1194becd8f7176bff2e511a5086b771dd35d7ec7
dungtran211096 <dungtran2196@gmail.com>
2018-09-17 11:09:20 +0700

    coding

---
+++ code/readfile.py
@@ -0,0 +1,20 @@
+from multiprocessing import Pool
+
+
+def process_line(line):
+    return line.strip().split(',')
+
+
+class ReadFile(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_file(self):
+        pool = Pool(4)
+        with open(self.filename) as source_file:
+            # chunk the work into batches of 4 lines at a time
+            results = pool.map(process_line, source_file, 4)
+            return results
+
+
